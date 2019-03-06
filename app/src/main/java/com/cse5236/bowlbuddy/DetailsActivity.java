26ea8ea7d4b58149a40a141824a5a24496b8a06f
lenzottij <lenzotti.5@buckeyemail.osu.edu>
2019-03-05 21:27:32 -0500

    working picasso slider

--- app/src/main/java/com/cse5236/bowlbuddy/DetailsActivity.java
+++ app/src/main/java/com/cse5236/bowlbuddy/DetailsActivity.java
@@ -3,6 +3,7 @@ package com.cse5236.bowlbuddy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
--- app/src/main/java/com/cse5236/bowlbuddy/DetailsActivityFragment.java
+++ app/src/main/java/com/cse5236/bowlbuddy/DetailsActivityFragment.java
@@ -2,6 +2,7 @@ package com.cse5236.bowlbuddy;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +12,24 @@ import android.view.ViewGroup;
  */
 public class DetailsActivityFragment extends Fragment {
 
+    private String[] imageUrls = new String[] {
+            "https://via.placeholder.com/150/0C0BFF/808080?Text=test1",
+            "https://via.placeholder.com/200/BBBBBF/808080?Text=test2",
+            "https://via.placeholder.com/100/0000AF/808080?Text=test3"
+    };
+
     public DetailsActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_details, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_details, container, false);
+        ViewPager viewPager = view.findViewById(R.id.view_pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity(), imageUrls);
+        viewPager.setAdapter(adapter);
+
+        return view;
     }
 }
