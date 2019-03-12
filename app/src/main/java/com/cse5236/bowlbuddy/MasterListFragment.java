ce646738f6f2d4eaac2806daacbb5563b9cf1d69
Jacob Patterson <patterson.882@osu.edu>
2019-03-11 21:48:40 -0400

    Added pullout DrawerLayout for navigation.
    + Skip login/logout (LoadingScreenActivity) if user is logged in
    + Implemented user logout
    + Cleaned up strings file
    - Removed some unneeded imports

--- app/src/main/java/com/cse5236/bowlbuddy/MasterListFragment.java
+++ app/src/main/java/com/cse5236/bowlbuddy/MasterListFragment.java
@@ -2,15 +2,18 @@ package com.cse5236.bowlbuddy;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.AppCompatRatingBar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -20,20 +23,18 @@ import android.widget.Toast;
 import com.cse5236.bowlbuddy.util.Bathroom;
 import com.cse5236.bowlbuddy.util.FakeData;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MasterListFragment extends Fragment {
+public class MasterListFragment extends Fragment implements NavigationView.OnNavigationItemSelectedListener {
     private final static String TAG = MasterListFragment.class.getSimpleName();
 
     private RecyclerView bathroomRecyclerView;
     private RecyclerView.Adapter bathroomAdapter;
     private RecyclerView.LayoutManager bathroomLayoutManager;
-    private List<Bathroom> bathroomList;
 
     public MasterListFragment() {
         // Required empty public constructor
@@ -54,18 +55,45 @@ public class MasterListFragment extends Fragment {
 
         bathroomRecyclerView.setLayoutManager(bathroomLayoutManager);
 
+        NavigationView nav = v.findViewById(R.id.master_nav_view);
+        nav.setNavigationItemSelectedListener(this);
+
         updateUI();
 
         Log.d(TAG, "onCreateView: View successfully created");
         return v;
     }
 
-    public void updateUI() {
+    private void updateUI() {
         FakeData data = new FakeData();
         data.generateData(5);
 
         bathroomAdapter = new BathroomAdapter(data.getBathrooms());
         bathroomRecyclerView.setAdapter(bathroomAdapter);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.log_out:
+                logOutAction();
+                return true;
+        }
+
+        return true;
+    }
+
+    /**
+     * "Logs out" a user, by clearing any "Session" Shared Preferences.
+     */
+    private void logOutAction() {
+        getContext().getSharedPreferences("Session", Context.MODE_PRIVATE)
+                .edit().clear().apply();
+
+        Intent i = new Intent(getActivity(), LoadingScreenActivity.class);
+        startActivity(i);
+
+        getActivity().finish();
     }
 
     private class BathroomHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
