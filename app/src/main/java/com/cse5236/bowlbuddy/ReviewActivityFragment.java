f1b7ca8bdacb088f77e7ab2a8562446c674d0460
lenzottij <lenzotti.5@buckeyemail.osu.edu>
2019-03-25 17:53:24 -0400

    Setup for posting reviews

--- app/src/main/java/com/cse5236/bowlbuddy/ReviewActivityFragment.java
+++ app/src/main/java/com/cse5236/bowlbuddy/ReviewActivityFragment.java
@@ -5,11 +5,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ReviewActivityFragment extends Fragment {
+
+    private View viewVar;
+    private RatingBar smellBar;
+    private RatingBar quietBar;
+    private RatingBar cleanBar;
+
+    int smellStars;
+    int quietStars;
+    int cleanStars;
 
     public ReviewActivityFragment() {
     }
@@ -17,6 +27,32 @@ public class ReviewActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_review, container, false);
+        viewVar = inflater.inflate(R.layout.fragment_review, container, false);
+        RatingBar smellBar = viewVar.findViewById(R.id.smellRating);
+        RatingBar quietBar = viewVar.findViewById(R.id.cleanRating);
+        RatingBar cleanBar = viewVar.findViewById(R.id.quietRating);
+        smellBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                smellStars = (int) rating;
+            }
+        });
+
+        quietBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                quietStars = (int) rating;
+            }
+        });
+
+        cleanBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                cleanStars = (int) rating;
+            }
+        });
+
+        return viewVar;
     }
+
 }
