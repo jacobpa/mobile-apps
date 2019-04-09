d272e1cd97359d675f8e433240e384fad0c11a0e
Eric Stevens <emstevens94@gmail.com>
2019-04-08 20:46:03 -0400

    Made location stuff "non internet" safe. Added landscape view for master list activity and fragment.

--- app/src/main/java/com/cse5236/bowlbuddy/MasterListActivity.java
+++ app/src/main/java/com/cse5236/bowlbuddy/MasterListActivity.java
@@ -1,6 +1,7 @@
 package com.cse5236.bowlbuddy;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v4.app.Fragment;
@@ -27,6 +28,8 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Response;
+
+import static android.support.v4.print.PrintHelper.ORIENTATION_PORTRAIT;
 
 public class MasterListActivity extends AppCompatActivity {
     private final static String TAG = MasterListActivity.class.getSimpleName();
@@ -55,12 +58,17 @@ public class MasterListActivity extends AppCompatActivity {
         // Get instance of current activity
         view = findViewById(R.id.activity_master_list);
 
-        // Manually set action bar, with menu button
+        //  Manually set action bar, with menu button
         Toolbar tb = findViewById(R.id.toolbar);
         setSupportActionBar(tb);
-        ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu_white);
+
+        // Only load hamburger menu if in portrait mode
+        // For some reason, portrait and landscape are reversed so negation is used
+        if (getResources().getConfiguration().orientation != ORIENTATION_PORTRAIT) {
+            ActionBar ab = getSupportActionBar();
+            ab.setDisplayHomeAsUpEnabled(true);
+            ab.setHomeAsUpIndicator(R.drawable.ic_menu_white);
+        }
 
         // Get an instance of the fragment manager
         FragmentManager fm = getSupportFragmentManager();
@@ -108,11 +116,13 @@ public class MasterListActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // If populated in onCreate, returns null object, wait until created in fragment
-        DrawerLayout drawerLayout = findViewById(R.id.master_drawer);
 
         switch(item.getItemId()) {
             // The menu bar on the top left was selected
             case android.R.id.home:
+                // This option will only happen in portrait mode
+                DrawerLayout drawerLayout = findViewById(R.id.master_drawer);
+
                 if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
                     drawerLayout.closeDrawer(GravityCompat.START);
                 } else {
@@ -136,7 +146,7 @@ public class MasterListActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy: Successfully ended activity");
-        finish();
+        //finish();
     }
 
     protected void onPause() {
