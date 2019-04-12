7668e5785f57047bd8dca923a2021f18f6d7745b
Jacob Patterson <jacob@jacobpa.com>
2019-04-12 17:46:49 -0400

    DetailsActivity/Fragment fixes
    + Change fragment loading to fragment manager method, from XML layout method
    - Remove finish() call in onDestroy()

--- app/src/main/java/com/cse5236/bowlbuddy/DetailsActivity.java
+++ app/src/main/java/com/cse5236/bowlbuddy/DetailsActivity.java
@@ -3,18 +3,33 @@ package com.cse5236.bowlbuddy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
 public class DetailsActivity extends AppCompatActivity {
-    private final static String TAG = LoadingScreenActivity.class.getSimpleName();
+    private final static String TAG = DetailsActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        // Get an instance of the fragment manager
+        FragmentManager fm = getSupportFragmentManager();
+
+        // Try to find the Fragment if it's already been created
+        Fragment fragment = fm.findFragmentById(R.id.details_fragment_container);
+
+        if(fragment == null) {
+            fragment = new DetailsActivityFragment();
+            fm.beginTransaction()
+                    .add(R.id.details_fragment_container, fragment)
+                    .commit();
+        }
 
         Log.d(TAG, "onCreate: Successfully created");
     }
@@ -22,7 +37,6 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy: Successfully ended activity");
-        finish();
     }
 
     protected void onPause() {
